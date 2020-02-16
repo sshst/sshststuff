a625d8b8d83bb5d87146f70e01e8e8af0572e716
Aidan Steele <aidan.steele@glassechidna.com.au>
2020-02-16 18:14:29 +1100

    auto title and notify user

--- cmd/listen.go
+++ cmd/listen.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"gopkg.in/src-d/go-git.v4"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/sshst/sshststuff"
@@ -39,7 +41,12 @@ func listenCmd() *cobra.Command {
 }
 
 func listen(cmd *cobra.Command, args []string) {
+	if len(listenConfig.NotifyUser) == 0 {
+		listenConfig.NotifyUser = headCommitAuthor()
+	}
+
 	listenConfig.Version = version
+	
 	err := client.Listen(context.Background(), listenConfig)
 	if err != nil {
 		panic(err)
@@ -52,7 +59,37 @@ func codebuild(cmd *cobra.Command, args []string) {
 		return
 	}
 
+	if len(listenConfig.NotifyTitle) == 0 {
+		id := os.Getenv("CODEBUILD_BUILD_ID")
+		split := strings.SplitN(id, ":", 2)
+		listenConfig.NotifyTitle = split[0]
+	}
+
 	listen(cmd, args)
+}
+
+func headCommitAuthor() string {
+	repo, err := git.PlainOpen(".")
+	if err != nil {
+		return ""
+	}
+
+	ref, err := repo.Head()
+	if err != nil {
+		return ""
+	}
+
+	cIter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	if err != nil {
+		return ""
+	}
+
+	commit, err := cIter.Next()
+	if err != nil {
+		return ""
+	}
+
+	return commit.Author.Email
 }
 
 func github(cmd *cobra.Command, args []string) {
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/sshst/sshststuff
 go 1.13
 
 require (
-	github.com/anmitsu/go-shlex v0.0.0-20161002113705-648efa622239 // indirect
 	github.com/gliderlabs/ssh v0.2.2
 	github.com/golang/protobuf v1.3.3
 	github.com/gorilla/websocket v1.4.1
@@ -12,7 +11,8 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/soheilhy/cmux v0.1.4
 	github.com/spf13/cobra v0.0.5
-	github.com/stretchr/testify v1.2.2
+	github.com/stretchr/testify v1.3.0
 	golang.org/x/crypto v0.0.0-20200214034016-1d94cc7ab1c6
 	google.golang.org/grpc v1.27.1
+	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
 )
